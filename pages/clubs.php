f37600ad6f3740a2e238dbae9f371fe60fff7ef0
scanbasket <103220694+medericc@users.noreply.github.com>
2024-03-19 06:50:37 +0100

    Update clubs.php

--- pages/clubs.php
+++ pages/clubs.php
@@ -13,7 +13,7 @@ include '../login/login.php';
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../sass/styles.css">
-    <title>Lucile Jerome</title>
+    <title>Thomas Radonda</title>
   
 </head>
 <body>
